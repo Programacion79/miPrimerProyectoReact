024fcc680f0ecfe1f4d813d6d5d3c235000d9fa4
Programacion79 <Programacion1979>
2025-04-11 05:39:53 -0500

    Agrego componentes nuevos y modifico archivos principales

---
+++ mi-proyecto/src/components/Content.jsx
@@ -0,0 +1,19 @@
+
+
+const Content = () => (
+  <div className="col-sm-8">
+    <h2>TITLE HEADING</h2>
+    <h5>Title description, Dec 7, 2020</h5>
+    <div className="fakeimg">Fake Image</div>
+    <p>Some text..</p>
+    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
+
+    <h2 className="mt-5">TITLE HEADING</h2>
+    <h5>Title description, Sep 2, 2020</h5>
+    <div className="fakeimg">Fake Image</div>
+    <p>Some text..</p>
+    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
+  </div>
+);
+
+export default Content;
---
+++ mi-proyecto/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <div className="mt-5 p-4 bg-dark text-white text-center">
+    <p>Footer</p>
+  </div>
+);
+
+export default Footer;
